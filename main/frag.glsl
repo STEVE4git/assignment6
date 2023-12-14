590db6f8a3ac238d5ec5b0d80df6d8de47228b07
Stephen <stephenmartinemail2003@gmail.com>
2023-12-13 21:57:34 -0500

    test

--- main/frag.glsl
+++ main/frag.glsl
@@ -36,5 +36,5 @@ vec3 diffuse = diff * lightColor;
 
     // Final color calculation
     vec3 result = ambient + (diffuse * textureColor) + specular;
-    fragment_color = textureColor;
+    fragment_color = vec4(textureColor,1.0f);
 }
