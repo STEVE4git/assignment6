233bc37c6147c9065abeab913e5ea56d652d0f54
Stephen <stephenmartinemail2003@gmail.com>
2023-12-13 21:49:35 -0500

    test

--- main/stub.cpp
+++ main/stub.cpp
@@ -222,6 +222,13 @@ void drawSurfaceOfRevolution() {
   glBindVertexArray(0);
   glBindVertexArray(1);
   glBindVertexArray(2);
+  int width, height, nrChannels;
+  unsigned char* data = stbi_load("atlas.jpg", &width, &height, &nrChannels, 0);
+  if (data) {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+                 GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  }
   glBindVertexArray(vbo);
   glBindVertexArray(ebo);
   glActiveTexture(GL_TEXTURE0);
