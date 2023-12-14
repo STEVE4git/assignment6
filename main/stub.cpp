87718beb8fae7c599ae74793e301c81291c21827
Stephen <stephenmartinemail2003@gmail.com>
2023-12-13 22:04:40 -0500

    test

--- main/stub.cpp
+++ main/stub.cpp
@@ -224,7 +224,8 @@ void drawSurfaceOfRevolution() {
   glBindVertexArray(1);
   glBindVertexArray(2);
   int width, height, nrChannels;
-  unsigned char* data = stbi_load("atlas.jpg", &width, &height, &nrChannels, 0);
+  unsigned char* data =
+      stbi_load("checkers.png", &width, &height, &nrChannels, 0);
   if (data) {
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
                  GL_UNSIGNED_BYTE, data);
